0a9d66d087f3a321292e53a490ce5043982a87f3
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2024-10-28 22:33:43 -0700

    Serialize Lean Document

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,6 +4,7 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 import profileDefault from '@/assets/images/profile.png'
 import ProfileProperties from "@/components/ProfileProperties";
+import { convertToSerializableObject } from "@/utils/convertToObject";
 
 const ProfilePage = async () => {
     await connectDB()
@@ -18,7 +19,9 @@ const ProfilePage = async () => {
         )
     }
 
-    const properties = await Property.find({owner: userId}).lean()
+    const propertiesDocs = await Property.find({owner: userId}).lean()
+
+    const properties = propertiesDocs.map(convertToSerializableObject)
 
     return ( 
         <section className="bg-blue-50">
